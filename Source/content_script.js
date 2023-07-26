d140795b7f1dd4d204db008bc82ced9b7c836855
ciczan <ciczan@gmail.com>
2023-07-26 17:05:22 +0200

    Replacing the replaces

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bChatGPT\b/g, "Monkey");
+	v = v.replace(/\bGPT-4\b/g, "Monkey-4");
+	v = v.replace(/\bAI Chatbot\b/g, "Talking Monkey");
+	v = v.replace(/\bAI chatbot\b/g, "talking monkey");
 	
 	textNode.nodeValue = v;
 }
